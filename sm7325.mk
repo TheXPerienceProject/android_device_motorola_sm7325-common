02052ee45e3b24d0fec74ceaae41b45be0e94a74
klozz <carlosj@klozz.dev>
2023-05-13 03:41:12 +0000

    sm7325-common: drop duplicated boot_jars
    Signed-off-by: klozz <carlosj@klozz.dev>

--- sm7325.mk
+++ sm7325.mk
@@ -445,9 +445,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils_prd.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
 
