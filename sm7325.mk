7b904c20558882ca83f08689429d8a9ee64966aa
klozz <carlosj@klozz.dev>
2023-06-03 00:50:00 +0000

    Revert "sm7350-common: use bootctr from hardware qcom and upgrade it to 1.2"
    This reverts commit 81b2bb1f2d657004877dd029ef5c35b939dbc98d.

--- sm7325.mk
+++ sm7325.mk
@@ -72,9 +72,11 @@ PRODUCT_COPY_FILES += \
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service \
+    bootctrl.lahaina \
+    bootctrl.lahaina.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
