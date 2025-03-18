fff39576ec7a7dd56b45a73bab81e5d8afc014f2
klozz <carlosj@klozz.dev>
2025-03-18 04:17:33 +0000

    sm7325-common: fix build issues
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,7 +110,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/xperience/config/device_framework_matrix.xml
+    vendor/xperience/interfaces/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 ifeq ($(TARGET_HAS_SNXXX_NFC),)
