8f075477a4b4905dde443e465aeeec41fef7f199
Michael Bestas <mkbestas@lineageos.org>
2025-03-31 20:44:08 +0300

    sm7325-common: Remove unused RFS MDM MPSS symlink
    Change-Id: Ib61df731835f70e566308c272d56430fb00c1531
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -306,10 +306,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rfs_mdm_mpss_readonly_vendor_fsg_symlink
 
-# RFS MSM MPSS symlinks
-PRODUCT_PACKAGES += \
-    rfs_msm_mpss_readonly_vendor_fsg_symlink
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.multihal
